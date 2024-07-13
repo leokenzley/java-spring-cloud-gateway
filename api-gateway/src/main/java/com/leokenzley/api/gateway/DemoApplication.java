733f2f4befed97cb42dc349f2d593d8a7f5f01fb
Leo Kenzley <leokenzley@gmail.com>
2024-07-12 21:07:13 -0300

    Adicionando o eureka

--- api-gateway/src/main/java/com/leokenzley/api/gateway/DemoApplication.java
+++ api-gateway/src/main/java/com/leokenzley/api/gateway/DemoApplication.java
@@ -9,10 +9,8 @@ import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterF
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 	@Autowired
 	private TokenRelayGatewayFilterFactory filterFactory;
