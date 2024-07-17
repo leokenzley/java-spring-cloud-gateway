f74d31343176adc30bac170c763d108f54317cf1
Leo Kenzley <leokenzley@gmail.com>
2024-07-16 21:34:58 -0300

    Adicionando o plugin do github para retornar os dados do commit da aplicação 'deployada'

--- callme/src/main/java/com/leokenzley/callme/infrastructure/security/SecurityConfig.java
+++ callme/src/main/java/com/leokenzley/callme/infrastructure/security/SecurityConfig.java
@@ -15,7 +15,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
+                        .requestMatchers(
+                                "/actuator/health",
+                                "/actuator/metrics",
+                                "/actuator/info",
+                                "/actuator/metrics/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/api/monitor/**",
+                                "/api/authentication/**").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
